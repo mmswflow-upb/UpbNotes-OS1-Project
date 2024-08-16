8beb157fd2a79828a30321213c9d02d8b6e8a308
Sakka Mohamad-Mario <152873071+mmswflow-upb@users.noreply.github.com>
2024-08-17 02:05:12 +0300

    Update db_creation_script.sql

--- db_creation_script.sql
+++ db_creation_script.sql
@@ -1,10 +1,6 @@
--- Create the database
 CREATE DATABASE UpbNotes;
-
--- Use the database
 USE UpbNotes;
 
--- Create the table `notes`
 CREATE TABLE notes (
     id INT(11) NOT NULL AUTO_INCREMENT,
     subject VARCHAR(200) NULL,
@@ -12,3 +8,8 @@ CREATE TABLE notes (
     file VARCHAR(1000) NULL,
     PRIMARY KEY (id)
 );
+
+CREATE USER 'USERNAME'@'127.0.0.1' IDENTIFIED BY '1234567';
+GRANT ALL PRIVILEGES ON UpbNotes.* TO 'USERNAME'@'127.0.0.1';
+FLUSH PRIVILEGES;
+EXIT;
